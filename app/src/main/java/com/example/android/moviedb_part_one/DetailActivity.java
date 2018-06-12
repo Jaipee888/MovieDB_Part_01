670291175ac18414e3e9e4a4fafc4179efbb2ffb
jaypr_000 <jayprakash888@gmail.com>
2018-06-11 23:11:00 -0700

    1. Added AsyncTask for Detail Activity. 2. Created a new Layout for detail Activity 3. Fixed Detail Activity to show the ImageView and TextView.

--- app/src/main/java/com/example/android/moviedb_part_one/DetailActivity.java
+++ app/src/main/java/com/example/android/moviedb_part_one/DetailActivity.java
@@ -1,40 +1,13 @@
 package com.example.android.moviedb_part_one;
 
-import android.content.Intent;
-import android.os.AsyncTask;
+
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.text.Html;
-import android.util.Log;
-import android.view.View;
-import android.widget.Adapter;
 import android.widget.ImageView;
-import android.widget.ProgressBar;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.widget.Toast;
-
 import com.squareup.picasso.Picasso;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-
-import okhttp3.MediaType;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.RequestBody;
-import okhttp3.Response;
 
 
 public class DetailActivity extends AppCompatActivity {
